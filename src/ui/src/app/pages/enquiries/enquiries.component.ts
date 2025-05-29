2467ca7a8348a26a29003405b2b344022e589000
Ryan De Wet <ryan.dewet.rdw@outlook.com>
2025-05-29 16:49:59 +0200

    init

--- src/ui/src/app/pages/enquiries/enquiries.component.ts
+++ src/ui/src/app/pages/enquiries/enquiries.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-enquiries',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './enquiries.component.html',
   styleUrl: './enquiries.component.scss'
 })
