0f11e5020206c7cc93c3ba3be2f62fc1a379f9c3
Ryan De Wet <ryan.dewet.rdw@outlook.com>
2025-05-29 21:07:16 +0200

    added back to module

--- src/ui/src/app/pages/enquiries/enquiries.component.ts
+++ src/ui/src/app/pages/enquiries/enquiries.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-enquiries',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './enquiries.component.html',
   styleUrl: './enquiries.component.scss'
 })
