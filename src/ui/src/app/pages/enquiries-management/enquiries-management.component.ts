eac4c19ed2d7a53afb8865cc14ddeaa6b1bab8bd
Ryan De Wet <93371328+TasmanianTiger48@users.noreply.github.com>
2025-05-30 16:54:23 +0200

    Marc/enquiries (#4)
    * enquiries setup
    
    * init
    
    * Fix popup
    
    * added back to module
    
    * inquiries pages
    
    * cleanup
    
    * cleanup
    
    * additional cleanup
    
    ---------
    
    Co-authored-by: Marc Picard <marc1pcard1@gmail.com>

--- src/ui/src/app/pages/enquiries-management/enquiries-management.component.ts
+++ src/ui/src/app/pages/enquiries-management/enquiries-management.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-enquiries-management',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './enquiries-management.component.html',
   styleUrl: './enquiries-management.component.scss'
 })
 export class EnquiriesManagementComponent {
+  isViewModalOpen: boolean = false;
+selectedInquiry: any = {};
+
+openViewModal(inquiry: any) {
+  this.selectedInquiry = { ...inquiry };
+  this.isViewModalOpen = true;
+}
+
+closeViewModal() {
+  this.isViewModalOpen = false;
+}
+
 
 }
