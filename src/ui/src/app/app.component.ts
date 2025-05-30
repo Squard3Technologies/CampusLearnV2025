b300e03d7e44a3a700fb143e33428445e5c3faa0
Ryan De Wet <ryan.dewet.rdw@outlook.com>
2025-05-30 16:52:26 +0200

    cleanup

--- src/ui/src/app/app.component.ts
+++ src/ui/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
